28d99cd1f6b1831dadec0717b5a17a48b64c0f9f
rachelseaborn <raseaborn@gmail.com>
2021-03-04 19:17:36 -0700

    front end in progress

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import "./index.css";
+import App from "./App";
+import Auth from "./Components/Auth/Auth";
+import Dash from "./Components/Dash/Dash";
+import Form from "./Components/Form/Form";
+import Post from "./Components/Post/Post";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
