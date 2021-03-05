7a2f5bd54c0b6e6a05e644ff77103081efc56a29
rachelseaborn <raseaborn@gmail.com>
2021-03-04 20:51:54 -0700

    working on front end at reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,14 +1,35 @@
 const { useReducer } = require("react");
 
+const UPDATE_USER = "UPDATE_USER";
+const LOGOUT_USER = "LOGOUT_USER";
+
 const initialState = {
-  username: "",
-  profile_pic: "",
+  user: {},
 };
+
+export function updateUser(userObj) {
+  return {
+    type: UPDATE_USER,
+    payload: userObj,
+  };
+}
+
+export function logout(userObj) {
+  return {
+    type: LOGOUT_USER,
+    payload: {},
+  };
+}
 
 export default function reducer(state = initialState, action) {
   const { type, payload } = action;
 
   switch (type) {
+    case UPDATE_USER:
+      return { ...state, user: payload };
+    case LOGOUT_USER:
+      return { ...state, user: payload };
+
     default:
       return state;
   }
